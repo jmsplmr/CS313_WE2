4232c6c3f566e4cdf755e84234ef7b8cd855b035
James Palmer <jms.plmr@gmail.com>
2018-05-03 16:26:40 -0600

    attempt some js

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -3,5 +3,6 @@
           <a href="home.php">Home</a>
           <a href="about-us.php">About us</a>
           <a href="login.php" >Login</a>
+          <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
         </div>';
 ?>
