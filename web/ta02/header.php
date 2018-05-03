952498925afeb867c89468b0cc30661374115b3a
James Palmer <jms.plmr@gmail.com>
2018-05-03 16:34:42 -0600

    Add active tests for header

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,11 +1,17 @@
 <?php
-  echo basename($_SERVER['PHP_SELF']);
-  // echo '<div class="topnav">
-  //         <a ';
-  // if basename($_SERVER['PHP_SELF']);
-  //         href="home.php">Home</a>
-  //         <a href="about-us.php">About us</a>
-  //         <a href="login.php" >Login</a>
-  //         <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-  //       </div>';
+  echo '<div class="topnav">
+          <a ';
+  if (basename($_SERVER['PHP_SELF']) == "home.php")
+    echo 'class="active"';
+  echo 'href="home.php">Home</a>
+          <a'; 
+  if (basename($_SERVER['PHP_SELF']) == "about-us.php")
+    echo 'class="active"';
+  echo ' href="about-us.php">About us</a>
+         <a ';
+  if (basename($_SERVER['PHP_SELF']) == "login.php")
+    echo 'class="active"';
+  echo 'href="login.php" >Login</a>';
+  echo '<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>';
 ?>
