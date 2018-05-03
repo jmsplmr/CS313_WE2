4a2fedf027b3b8999bc4bcbc9de917cf577e6ec4
James Palmer <jms.plmr@gmail.com>
2018-05-03 16:29:23 -0600

    test basename

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,8 +1,11 @@
 <?php
-  echo '<div class="topnav">
-          <a href="home.php">Home</a>
-          <a href="about-us.php">About us</a>
-          <a href="login.php" >Login</a>
-          <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-        </div>';
+  echo basename($_SERVER['PHP_SELF']);
+  // echo '<div class="topnav">
+  //         <a ';
+  // if basename($_SERVER['PHP_SELF']);
+  //         href="home.php">Home</a>
+  //         <a href="about-us.php">About us</a>
+  //         <a href="login.php" >Login</a>
+  //         <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+  //       </div>';
 ?>
