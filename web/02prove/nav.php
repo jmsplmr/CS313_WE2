0a052299be4e86b65cc8f0c9940ed6f74ccad149
James Palmer <jms.plmr@gmail.com>
2018-05-06 11:02:40 -0600

    Correct usage

--- web/02prove/nav.php
+++ web/02prove/nav.php
@@ -3,5 +3,4 @@
     <a href="home.php" <?php if ($uri == "/02prove/home.php") { echo "class='active'";} else {echo "class='not-active'";} ?>>Home</a>
     <a href="about.php" <?php if ($uri == "/02prove/about.php") { echo "class='active'";} else {echo "class='not-active'";} ?>>About</a>
     <a href="assignments.php" <?php if ($uri == "/02prove/assignments.php") { echo "class='active'";} else {echo "class='not-active'";} ?>>Assignments</a>
-    <a href="contact.php" <?php if ($uri == "/02prove/contact.php") { echo "class='active'";} else {echo "class='not-active'";} ?>>Contact</a>
 </nav>
--- web/ta02/login.php
+++ web/ta02/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +9,7 @@
   <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
   <script src="main.js"></script>
 </head>
+
 <body>
   <?php
     require("header.php");
@@ -21,8 +23,7 @@
     if (isset($_POST['tester'])) { 
       $_session['tester'] = $_POST['tester'];
     }
-    
-    bool isset ( mixed $var [, mixed $... ] )
   ?>
 </body>
+
 </html>
