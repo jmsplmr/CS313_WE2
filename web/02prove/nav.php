d7caa5cfb3ef56d41952bf2e53e7e39cbff1ee5e
James Palmer <jms.plmr@gmail.com>
2018-05-05 22:45:28 -0600

    Correct colors

--- web/02prove/nav.php
+++ web/02prove/nav.php
@@ -2,6 +2,6 @@
     <?php $uri = $_SERVER['REQUEST_URI']; ?>
     <a href="home.php" <?php if ($uri == "/02prove/home.php") { echo "class='active'";} ?>>Home</a>
     <a href="about.php" <?php if ($uri == "/02prove/about.php") { echo "class='active'";} ?>>About</a>
-    <a href="assign.php" <?php if ($uri == "/02prove/Assignments.php") { echo "class='active'";} ?>>Assignments</a>
+    <a href="assignments.php" <?php if ($uri == "/02prove/assignments.php") { echo "class='active'";} ?>>Assignments</a>
     <a href="contact.php" <?php if ($uri == "/02prove/contact.php") { echo "class='active'";} ?>>Contact</a>
 </nav>
