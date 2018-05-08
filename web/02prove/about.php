0296283b65a0da5504ba1fed4291af7327e1fea4
James Palmer <jms.plmr@gmail.com>
2018-05-08 15:51:08 -0600

    Try line height

--- web/02prove/about.php
+++ web/02prove/about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,23 +9,26 @@
   <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
   <script src="main.js"></script>
 </head>
+
 <body>
   <?php
     require "nav.php";
   ?>
 
-  <h1>
     <div>
+      <h1>
+        About Me | James Palmer
+      </h1>
       <p class="text">
-        Its is now my final year of school. I am studying Computer Science and I have 3 semesters until I graduate. T I have been married for 3 and half years 
-        to Jessica. We have an adorable 19 month old. In December we will be welcoming another new one whose gender remains to be revealed to us. I love board 
-        games, ultimate, tv, and reading. Though i have a hard time getting to fiction during school my favorite right now is Patrick Rothfuss but he needs to 
-        get working on the next book in his Name of the Wind series. 
-        <br/>
-        My family and I spent spring break in Logan, Utah where my wife's family now lives. It was a wonderful, much needed break. I succeeded in doing 
-        nothing. We love to spend time with family.
+        Its is now my final year of school. I am studying Computer Science and I have 3 semesters until I graduate. T I have been
+        married for 3 and half years to Jessica. We have an adorable 19 month old. In December we will be welcoming another
+        new one whose gender remains to be revealed to us. I love board games, ultimate, tv, and reading. Though i have a
+        hard time getting to fiction during school my favorite right now is Patrick Rothfuss but he needs to get working
+        on the next book in his Name of the Wind series.
+        <br/> My family and I spent spring break in Logan, Utah where my wife's family now lives. It was a wonderful, much needed
+        break. I succeeded in doing nothing. We love to spend time with family.
       </p>
     </div>
-  </h1>
 </body>
+
 </html>
