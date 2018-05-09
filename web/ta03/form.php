6d763d86884e559cdd994ea2846d7d8e67a681fd
James Palmer <jms.plmr@gmail.com>
2018-05-08 21:25:27 -0600

    Correct mailto:

--- web/ta03/form.php
+++ web/ta03/form.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
+  <title>Form Result</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
   <script src="main.js"></script>
@@ -15,7 +15,7 @@
   User:
   <?php echo $_POST["name"]; ?>
   <br> Email Address is:
-  <a href="mailto: <?php echo $_POST[" email "];?>">
+  <a href="mailto:<?php echo $_POST["email"];?>">
     <?php echo $_POST["email"];?>
   </a>
   <br> Major:
