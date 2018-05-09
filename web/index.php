574acb441bea00b90f3d58468f1389114f5ea72b
James Palmer <jms.plmr@gmail.com>
2018-05-09 09:08:04 -0600

    Make index point to home

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
-
-  phpinfo();
-
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/cs313_php/web/02prove/home.php');
+	exit;
 ?>
+Something is wrong with the XAMPP installation :-(
