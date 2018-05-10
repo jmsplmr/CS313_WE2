9a43e3851d70a9fce05cd0344ba582d93a2cf220
James Palmer <jms.plmr@gmail.com>
2018-05-10 15:39:04 -0600

    Index redirects localhost and heroku differently

--- web/index.php
+++ web/index.php
@@ -5,7 +5,13 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/02prove/home.php');
-	exit;
+	echo $_SERVER['SERVER_NAME'];
+	// if ($_SERVER['SERVER_NAME']=='localhost' . dirname($_SERVER['PHP_SELF']);) {
+	// 	header('Location: '.$uri.'/02prove/home.php');
+	// }
+	// elseif ($_SERVER['SERVER_NAME']=='infinite-river-43265.herokuapp.com') {
+	// 	header('Location: '.$uri.'/cs313_php/web/02prove/home.php');
+	// }
+	// exit;
 ?>
-Something is wrong with the XAMPP installation :-(
+	Something is wrong with the XAMPP installation :-(
