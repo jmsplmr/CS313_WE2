1573391c24a7964b956938e1ee8261c6f9dada4e
James Palmer <jms.plmr@gmail.com>
2018-05-22 16:44:52 -0600

    Stretch Search

--- web/ta05/search-scriptures.php
+++ web/ta05/search-scriptures.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
+  <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Scripture Results</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
   <script src="main.js"></script>
 </head>
 <body>
-  <h1>Scripture Results</h1>
-  
-  <?php
+<h1>Scripture Results</h1>
+<?php
   require '../db-credentials.php';
 
   $book = $_POST['book'];
 
-  $qry = $db->prepare('SELECT book, chapter, verse, content FROM Scriptures WHERE book=:book');
-  $qry->execute(array(':book' => $book));
-  $results = $qry->fetchAll(PDO::FETCH_ASSOC);
+  $qry = $db -> prepare('SELECT book, chapter, verse, content FROM Scriptures WHERE book=:book');
+  $qry -> execute([':book' => $book]);
+  $results = $qry -> fetchAll(PDO::FETCH_ASSOC);
 
   foreach ($results as $row) {
-    echo '<strong>'.$row['book'].' '.$row['chapter'].':'.$row['verse'].'</strong>';
-    echo ' - '.$row['content'].'</br>';
+    echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong>';
+    echo ' - ' . $row['content'] . '</br>';
   }
-  ?>
+?>
 </body>
 </html>
