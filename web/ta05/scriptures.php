584c721c667c29c9f492767d5e9df5e0d3011b30
James Palmer <jms.plmr@gmail.com>
2018-05-22 15:55:13 -0600

    Attempt to log into database

---
+++ web/ta05/scriptures.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Scripture Resources</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <script src="main.js"></script>
+</head>
+<body>
+  <h1>Scripture Resources</h1>
+  <?php
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbopts = parse_url($dbUrl);
+
+      $dbHost = $dbopts["host"];
+      $dbPort = $dbopts["port"];
+      $dbUser = $dbopts["user"];
+      $dbPassword = $dbopts["pass"];
+      $dbName = ltrim($dbopts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+      // this line makes PDO give us an exception when there are problems,
+      // and can be very helpful in debugging! (But you would likely want
+      // to disable it for production environments.)
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      echo '<p>We think it is successfull</p>';
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+  ?>
+</body>
+</html>
