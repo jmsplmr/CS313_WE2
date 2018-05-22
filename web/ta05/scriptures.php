ed95604f903f52aac5aa7c2e566eb4729212328c
James Palmer <jms.plmr@gmail.com>
2018-05-22 16:43:37 -0600

    Stretch Search

--- web/ta05/scriptures.php
+++ web/ta05/scriptures.php
@@ -3,19 +3,17 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Scripture Resources</title>
+  <title>Scripture Search</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
   <script src="main.js"></script>
 </head>
 <body>
-  <h1>Scripture Resources</h1>
-
-  <form action="search-scriptures.php.php">
-    <label for="book">Name:</label>
+  <h1>Scripture Search</h1>
+  <form action="search-scriptures.php">
+    <label for="book">Search for references by book:</label>
     <input type="search" name="book">
     <input type="submit"/>
   </form>
-
 </body>
 </html>
--- web/ta05/search-scriptures.php
+++ web/ta05/search-scriptures.php
@@ -1,8 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Scripture Results</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <script src="main.js"></script>
+</head>
+<body>
+  <h1>Scripture Results</h1>
+  
+  <?php
+  require '../db-credentials.php';
 
+  $book = $_POST['book'];
 
-  foreach ($db->query('SELECT book, chapter, verse, content FROM Scriptures') as $row) {
+  $qry = $db->prepare('SELECT book, chapter, verse, content FROM Scriptures WHERE book=:book');
+  $qry->execute(array(':book' => $book));
+  $results = $qry->fetchAll(PDO::FETCH_ASSOC);
 
+  foreach ($results as $row) {
     echo '<strong>'.$row['book'].' '.$row['chapter'].':'.$row['verse'].'</strong>';
     echo ' - '.$row['content'].'</br>';
   }
+  ?>
+</body>
+</html>
