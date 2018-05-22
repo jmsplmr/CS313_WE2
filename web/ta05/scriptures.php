142058be71479a7f71f585cd96e56efcd31eca39
James Palmer <jms.plmr@gmail.com>
2018-05-22 16:11:06 -0600

    Output scriptures

--- web/ta05/scriptures.php
+++ web/ta05/scriptures.php
@@ -38,7 +38,7 @@
       die();
     }
 
-    foreach ($db-query('SELECT book, chapter, verse, content FROM Scriptures') as $row) {
+    foreach ($db->query('SELECT book, chapter, verse, content FROM Scriptures') as $row) {
       echo '<strong>'.$row['book'].' '.$row['chapter'].':'.$row['verse'].'</strong>';
       echo ' - '.$row['content'].'</br>';
     }
