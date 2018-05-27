7731a46d931b916b6645666bc8fdca728846aa4a
James Palmer <jms.plmr@gmail.com>
2018-05-26 21:25:43 -0600

    Add registration page

--- web/05prove/authenticate.php
+++ web/05prove/authenticate.php
@@ -24,11 +24,11 @@
       $qry -> execute([':user' => $user, ':pswd' => $pswd]);
       $results = $qry_email -> fetchAll(PDO::FETCH_ASSOC);
 
-      //  if ($results[0]['username'] == $user) {
-      //    $_SESSION['logged_in'] = TRUE;
-      //  }
+      if ($results[0]['username'] == $user) {
+        $_SESSION['logged_in'] = TRUE;
+      }
 
-      if (!$results[0]) {
+      if ($results->rowCount() == 0) {
         echo '<p class="message">Username or password incorrect.</p>';
         echo '</br>';
       } else {
