18fdfc71f32b7cf52d96482d994c0471909efdfb
James Palmer <jms.plmr@gmail.com>
2018-05-26 21:25:43 -0600

    Add registration page

--- web/05prove/authenticate.php
+++ web/05prove/authenticate.php
@@ -1,7 +1,42 @@
+<!DOCTYPE html>
 <?php
-  /**
-   * Created by PhpStorm.
-   * User: James
-   * Date: 26-May-18
-   * Time: 6:32 PM
-   */
+  require '../db-credentials.php';
+
+  $user = $_POST['username'];
+  $pswd = $_POST['pswd'];
+
+  $qry = $db -> prepare('SELECT users.username FROM users WHERE username=user AND pswdhash = crypt(:pswd, pswdhash);');
+  $qry -> execute([':user' => $user, ':pswd' => $pswd]);
+  $results = $qry_email -> fetchAll(PDO::FETCH_ASSOC);
+
+  if ($results[0]) {
+    $_SESSION['loggedin'] = TRUE;
+  }
+?>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="login.css"/>
+  <script src="login.js"></script>
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+    <?php
+      if (!$results_usr[0]) {
+        echo '<p class="message">Username or password incorrect.</p>';
+        echo '</br>';
+      } else {
+        echo '<p class="message">Success: ' . $_SESSION['loggedin'] . '</p>';
+        echo '</br>';
+      }
+    ?>
+  </div>
+</div>
+
+</body>
+</html>
+
