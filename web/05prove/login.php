25059a981857f387441de21c1ea84794aa018893
James Palmer <jms.plmr@gmail.com>
2018-05-26 18:36:57 -0600

    Create login page

--- web/05prove/login.php
+++ web/05prove/login.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="utf-8" />
+  <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Disc Golf | Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
   <script src="main.js"></script>
 </head>
+
 <body>
-  <h1>Login</h1>
-  <form action="authenticate.php" method="post">
-    <input type="text" name="username" placeholder="username" id="username"><br>/
-    <input type="password" name="pswd_insecure" placeholder="password" id="pswd"><br>
-    <input type="submit" value="Submit">
-  </form>
+<div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name" required/>
+      <input type="password" placeholder="password" required/>
+      <input type="text" placeholder="email address" required/>
+      <button>create</button>
+      <p class="message">Already registered?
+        <a href="#">Sign In</a>
+      </p>
+    </form>
+    <form class="login-form" action="authenticate.php" method="post">
+      <input type="text" name="username" placeholder="username" id="username">
+      <input type="password" name="pswd_insecure" placeholder="password" id="pswd">
+      <input type="submit" value="Login">
+      <p class="message">Not registered?<a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
 </body>
+
 </html>
