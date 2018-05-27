e7aa21832318233d891fdec86f8cff515260eb7a
James Palmer <jms.plmr@gmail.com>
2018-05-26 18:36:57 -0600

    Create login page

--- web/05prove/login.php
+++ web/05prove/login.php
@@ -8,16 +8,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="login.css"/>
   <script src="login.js"></script>
+  <script src='cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 </head>
 
 <body>
 <div class="login-page">
   <div class="form">
     <form class="register-form" action="register.php">
-      <input type="text" name="name" id="name">
-      <input type="text" placeholder="name" required/>
-      <input type="password" placeholder="password" required/>
-      <input type="text" placeholder="email address" required/>
+      <input type="text" name="name" id="name" placeholder="name">
+      <input type="text" name="usrname" id="usrname" placeholder="username" required/>
+      <input type="password" name="pswd" id="pswd" placeholder="password" required/>
+      <input type="text" name="email" id="email" placeholder="email address" required/>
       <input type="submit" value="Create">
       <p class="message">Already registered?
         <a href="#">Sign In</a>
@@ -25,7 +26,7 @@
     </form>
     <form class="login-form" action="authenticate.php" method="post">
       <input type="text" name="username" placeholder="username" id="username">
-      <input type="password" name="pswd_insecure" placeholder="password" id="pswd">
+      <input type="password" name="pswd" placeholder="password" id="pswd">
       <input type="submit" value="Login">
       <p class="message">Not registered?
         <a href="#">Create an account</a>
