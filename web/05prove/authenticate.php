948895bf9eae9decab74805528593acf2b391011
James Palmer <jms.plmr@gmail.com>
2018-05-26 18:36:57 -0600

    Create login page

---
+++ web/05prove/authenticate.php
@@ -0,0 +1,7 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * User: James
+   * Date: 26-May-18
+   * Time: 6:32 PM
+   */
---
+++ web/05prove/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Disc Golf | Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <script src="main.js"></script>
+</head>
+<body>
+  <h1>Login</h1>
+  <form action="authenticate.php" method="post">
+    <label for="username"></label>
+    <input type="text" name="username" id="username">
+    <label for="pswd_insecure">Password</label>
+    <input type="password" name="pswd_insecure" id="pswd">
+    <input type="submit" value="">
+  </form>
+</body>
+</html>
