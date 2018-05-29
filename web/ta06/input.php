5e1cf39dcd74383676190e913929011c89988658
James Palmer <jms.plmr@gmail.com>
2018-05-29 16:35:10 -0600

    TA06 req 1-2

--- web/ta06/input.php
+++ web/ta06/input.php
@@ -1,11 +1,12 @@
 <?php
-  $book       = htmlspecialchars($_POST['book']);
-  $chapter    = htmlspecialchars($_POST['chapter']);
-  $verse      = htmlspecialchars($_POST['verse']);
-  $content    = htmlspecialchars($_POST['content']);
+  $book    = htmlspecialchars($_POST['book']);
+  $chapter = htmlspecialchars($_POST['chapter']);
+  $verse   = htmlspecialchars($_POST['verse']);
+  $content = htmlspecialchars($_POST['content']);
 
-  if (!empty($_POST['topics'])){
-    foreach ($_POST['topics'] as $topic){
+  if (!empty($_POST['topics'])) {
+    print_r($_POST['topics']);
+    foreach ($_POST['topics'] as $topic) {
       print_r($topic);
     }
   }
