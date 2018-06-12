ab20a28e86df1106b2a57ac1746173886fedd85c
James Palmer <jms.plmr@gmail.com>
2018-06-12 10:16:28 -0600

    Auth return name

--- web/05prove/status.php
+++ web/05prove/status.php
@@ -7,14 +7,16 @@
   $db = get_db();
 
   $qry
-      = $db -> prepare("SELECT name, street_address, city, state, rating FROM users u INNER JOIN course_rating r ON r.user_id = u.id INNER JOIN courses c ON c.id = r.course_id WHERE u.id = 5;");
+      = $db -> prepare("SELECT name, street_address, city, state, rating FROM users u INNER JOIN course_rating r ON r.user_id = u.id INNER 
+JOIN courses c ON c.id = r.course_id WHERE u.id = :id;");
   $qry -> bindValue(":id", $user, PDO::PARAM_INT);
   $qry -> execute();
   $courses = $qry -> fetchAll(PDO::FETCH_ASSOC);
 
   $_SESSION['course_ratings[]'] = $courses;
 
-  $qry_rounds = $db->prepare("SELECT score, g.name AS format, c2.name AS course, r.date FROM rounds r INNER JOIN courses c2 on r.course_id = c2.id INNER JOIN game_format g on r.format_id = g.id WHERE user_id=5 ORDER BY r.date;");
+  $qry_rounds = $db->prepare("SELECT score, g.name AS format, c2.name AS course, r.date FROM rounds r INNER JOIN courses c2 on r.course_id = c2.id 
+INNER JOIN game_format g on r.format_id = g.id WHERE user_id=:id ORDER BY r.date;");
   $qry_rounds -> bindValue(":id", $user, PDO::PARAM_INT);
   $qry_rounds->execute();
   $rounds = $qry_rounds->fetchAll(PDO::FETCH_ASSOC);
