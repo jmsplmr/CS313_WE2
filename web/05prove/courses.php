2ca6c3632b3ab39dc2a70cb630cfe3182305d1f8
James Palmer <jms.plmr@gmail.com>
2018-06-12 13:48:43 -0600

    Add courses

--- web/05prove/courses.php
+++ web/05prove/courses.php
@@ -33,19 +33,19 @@
   <h2>Add a Course</h2>
   <form method="post" action="add_course.php">
     <label for="name">Name: </label>
-    <input type="text" name="Name" id="name" placeholder="Disc Golf Course" required>
+    <input type="text" name="Name" id="name" placeholder="Disc Golf Course" required><br>
     <label for="phone">Phone #: </label>
-    <input type="text" name="phone" id="phone" placeholder="555-444-3333">
+    <input type="text" name="phone" id="phone" placeholder="555-444-3333"><br>
     <label for="contact">Contact: </label>
-    <input type="text" name="contact" id="contact" placeholder="Responsible Party">
+    <input type="text" name="contact" id="contact" placeholder="Responsible Party"><br>
     <label for="address">Address: </label>
-    <input type="text" name="address" id="address" placeholder="# D Street Abr." required>
+    <input type="text" name="address" id="address" placeholder="# D Street Abr." required><br>
     <label for="city">City: </label>
-    <input type="text" name="city" id="city" placeholder="Anywhere" required>
+    <input type="text" name="city" id="city" placeholder="Anywhere" required><br>
     <label for="state">State: </label>
-    <input type="text" name="state" id="state" placeholder="ST" required>
+    <input type="text" name="state" id="state" placeholder="ST" required><br>
     <label for="zip">Zip: </label>
-    <input type="text" name="zip" id="zip" placeholder="55555" required>
+    <input type="text" name="zip" id="zip" placeholder="55555" required><br>
     <input type="submit">
   </form>
 </div>
