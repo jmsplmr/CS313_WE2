03296a94963e5a6b7b37da65e3e809836a048aac
James Palmer <jms.plmr@gmail.com>
2018-06-12 14:20:32 -0600

    Add courses

--- web/05prove/add_course.php
+++ web/05prove/add_course.php
@@ -20,31 +20,32 @@
   $city    = htmlspecialchars($_POST['city']);
   $state   = htmlspecialchars($_POST['state']);
   $zip     = htmlspecialchars($_POST['zip']);
-  $rating  = htmlspecialchars($_POST['rating']);
+  $rating  = ($_POST['rating']);
 
-  $stmt = 'INSERT INTO courses (name, street_address, city, state, zip, phone, contact) VALUES (:name, :address, :city, :state, :zip, :phone, :contact);';
+  $stmt
+      = 'INSERT INTO courses (name, street_address, city, state, zip, phone, contact) VALUES (:name, :address, :city, :state, :zip, :phone, :contact);';
 
-  $insert = $db->prepare($stmt);
+  $insert = $db -> prepare($stmt);
 
-  $insert->bindParam(":name", $name, PDO::PARAM_STR);
-  $insert->bindParam(":address", $address, PDO::PARAM_STR);
-  $insert->bindParam(":city", $city, PDO::PARAM_STR);
-  $insert->bindParam(":state", $state, PDO::PARAM_STR);
-  $insert->bindParam(":zip", $zip, PDO::PARAM_STR);
-  $insert->bindParam(":phone", $phone, PDO::PARAM_STR);
-  $insert->bindParam(":contact", $contact, PDO::PARAM_STR);
+  $insert -> bindParam(":name", $name, PDO::PARAM_STR);
+  $insert -> bindParam(":address", $address, PDO::PARAM_STR);
+  $insert -> bindParam(":city", $city, PDO::PARAM_STR);
+  $insert -> bindParam(":state", $state, PDO::PARAM_STR);
+  $insert -> bindParam(":zip", $zip, PDO::PARAM_STR);
+  $insert -> bindParam(":phone", $phone, PDO::PARAM_STR);
+  $insert -> bindParam(":contact", $contact, PDO::PARAM_STR);
 
-  $insert->execute();
+  $insert -> execute();
 
-  $course_id = $db -> lastInsertId (['course_id_seq']);
+  $course_id = $db -> lastInsertId(['course_id_seq']);
 
   $stmt_rating = 'INSERT INTO course_rating(user_id,course_id,rating) VALUES (:user, :course, :rating);';
 
-  $create_rating = $db->prepare($stmt_rating);
-  $create_rating->bindParam(":user", $user, PDO::PARAM_STR);
-  $create_rating->bindParam(":course", $course_id, PDO::PARAM_STR);
-  $create_rating->bindParam(":rating", $rating, PDO::PARAM_INT);
-  $create_rating->execute();
+  $create_rating = $db -> prepare($stmt_rating);
+  $create_rating -> bindParam(":user", $user, PDO::PARAM_INT);
+  $create_rating -> bindParam(":course", $course_id, PDO::PARAM_INT);
+  $create_rating -> bindParam(":rating", $rating, PDO::PARAM_INT);
+  $create_rating -> execute();
 
   header('location: courses.php');
   die();
--- web/05prove/add_round.php
+++ web/05prove/add_round.php
@@ -17,14 +17,15 @@
   $score = htmlspecialchars($_POST['score']);
   $user = $_SESSION['user'];
 
-  $stmt = 'INSERT INTO rounds(course_id, user_id, format_id, score, date) VALUES (:course, :user, :format, :score, :date);';
+  $stmt
+      = 'INSERT INTO rounds(course_id, user_id, format_id, score, date) VALUES (:course, :user, :format, :score, :date);';
 
-  $insert = $db->prepare($stmt);
-  $insert->bindValue(":course", $course, PDO::PARAM_INT);
-  $insert->bindValue(":user", $user, PDO::PARAM_INT);
-  $insert->bindValue(":format", $format, PDO::PARAM_INT);
-  $insert->bindValue(":score", $score, PDO::PARAM_INT);
-  $insert->bindValue(":date", $date, PDO::PARAM_INT);
+  $insert = $db -> prepare($stmt);
+  $insert -> bindValue(":course", $course, PDO::PARAM_INT);
+  $insert -> bindValue(":user", $user, PDO::PARAM_INT);
+  $insert -> bindValue(":format", $format, PDO::PARAM_INT);
+  $insert -> bindValue(":score", $score, PDO::PARAM_INT);
+  $insert -> bindValue(":date", $date, PDO::PARAM_INT);
   $insert -> execute();
 
   header('location: rounds.php');
