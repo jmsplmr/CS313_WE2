821d6a6865b35832d2d21d26492c4507b87ad4a9
James Palmer <jms.plmr@gmail.com>
2018-06-12 11:54:33 -0600

    Update rounds

--- web/05prove/add_round.php
+++ web/05prove/add_round.php
@@ -5,7 +5,8 @@
    * Date: 12-Jun-18
    * Time: 11:26 AM
    */
-
+  ``
+  session_start();
   require_once '../dbConnect.php';
 
   $db = get_db();
@@ -16,18 +17,16 @@
   $score = $_POST['score'];
   $user = $_SESSION['user'];
 
-  echo $user;
-
   $stmt = 'INSERT INTO rounds(course_id, user_id, format_id, score, date) VALUES (:course, :user, :format, :score, :date);';
 
-//  $insert = $db->prepare($stmt);
-//  $insert->bindValue(":course", $course, PDO::PARAM_INT);
-//  $insert->bindValue(":user", $user, PDO::PARAM_INT);
-//  $insert->bindValue(":format", $format, PDO::PARAM_INT);
-//  $insert->bindValue(":score", $score, PDO::PARAM_INT);
-//  $insert->bindValue(":date", $date, PDO::PARAM_INT);
-//  $insert->execute();
-//
-//  header('location: rounds.php');
-//  die();
+  $insert = $db->prepare($stmt);
+  $insert->bindValue(":course", $course, PDO::PARAM_INT);
+  $insert->bindValue(":user", $user, PDO::PARAM_INT);
+  $insert->bindValue(":format", $format, PDO::PARAM_INT);
+  $insert->bindValue(":score", $score, PDO::PARAM_INT);
+  $insert->bindValue(":date", $date, PDO::PARAM_INT);
+  $insert->execute();
+
+  header('location: rounds.php');
+  die();
 
