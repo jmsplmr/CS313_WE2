0aa8c178616a48ad64240133a9a447b736d0c384
James Palmer <jms.plmr@gmail.com>
2018-06-12 02:51:21 -0600

    Trial of home

--- web/05prove/home.php
+++ web/05prove/home.php
@@ -1,7 +1,28 @@
 <!DOCTYPE html>
 <?php
   session_start();
-  include 'auth.php';
+  if ($_SESSION["auth"]) {
+    session_start();
+    require_once "../dbConnect.php";
+
+    $db = get_db();
+
+    $user_id = $_SESSION["user"];
+
+    $qry = $db->prepare("SELECT fullname, email FROM users WHERE id=:id;");
+    $qry->bindValue(":id", $user_id, PDO::PARAM_INT);
+    $qry->execute();
+    $results = $qry->fetch(PDO::FETCH_ASSOC);
+    echo $results;
+
+    $name = $results[0]['fullname'];
+    $_SESSION['name'] = $name;
+    echo $name;
+
+    $email = $results[0]['email'];
+    $_SESSION['email'] = $email;
+    echo $email;
+  }
 ?>
 <html>
 <head>
