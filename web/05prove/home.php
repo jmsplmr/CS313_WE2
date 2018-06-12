18433cb24db0005266ec103b0ee82a0094768024
James Palmer <jms.plmr@gmail.com>
2018-06-12 02:56:07 -0600

    Auth return name

--- web/05prove/home.php
+++ web/05prove/home.php
@@ -2,17 +2,14 @@
 <?php
   session_start();
   if ($_SESSION["auth"]) {
-    session_start();
-    require_once "../dbConnect.php";
-
-    $db = get_db();
+    require "../db-credentials.php";
 
     $user_id = $_SESSION["user"];
 
     $qry = $db->prepare("SELECT fullname, email FROM users WHERE id=:id;");
-    $qry->bindValue(":id", $user_id, PDO::PARAM_INT);
+    $qry->bindValue("id", $user_id, PDO::PARAM_INT);
     $qry->execute();
-    $results = $qry->fetch(PDO::FETCH_ASSOC);
+    $results = $qry->fetch(PDO::FETCH_LAZY);
     echo $results;
 
     $name = $results[0]['fullname'];
